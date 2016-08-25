fccbdf5106cb03efb76c65cf9de179b100002a1c
codecademy <ccuser@codecademy.com>
2016-08-25 09:29:43 -0400

    Work done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
   get '/about' => 'pages#about'
+  get '/thanks' => 'pages#thanks'
+  resources :signups
 end
---
+++ test/models/signu_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SignuTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
